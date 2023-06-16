96c04c50463a39b60ced7f5277d17414f83a67c4
Anita Kemp <akemp@esri.com>
2023-06-15 22:53:51 -0600

    Add openlayers traces

--- trace-script.js
+++ trace-script.js
@@ -16,14 +16,17 @@ const fs = require('fs');
   //     await browser.close();
   // })();
 
+  const loopIteration = 5;
+
+  for (let i=0; i < loopIteration; i++) {
   (async () => {
     const browser =  await puppeteer.launch()
     const page = await browser.newPage()
     await page.setCacheEnabled(false);
     await page.setViewport({ width: 1920, height: 1080 });
   
-    await page.tracing.start({ screenshots: true, path: './leaflet/vector-tiles/results/raster-vector/leaflet-trace-4.json' });
-    await page.goto('http://127.0.0.1:5501/leaflet/vector-tiles/raster-style-vector.html', {waitUntil: ['networkidle2'], timeout: 60000 });
+    await page.tracing.start({ screenshots: true, path: `./openlayers/vector-tiles/results/vector-vector/openlayers-trace-${i}.json` });
+    await page.goto('http://127.0.0.1:5501/openlayers/vector-tiles/vector-style-vector.html', {waitUntil: ['networkidle0'], timeout: 60000 });
     // const metrics = await page.evaluate(()=> JSON.stringify(window.performance));
     // console.log(JSON.parse(metrics))
     // const pageMetrics = await page.metrics();
@@ -31,7 +34,7 @@ const fs = require('fs');
     await page.tracing.stop();
   
     // Extract data from the trace
-    const tracing = JSON.parse(fs.readFileSync('./leaflet/vector-tiles/results/raster-vector/leaflet-trace-4.json' , 'utf8'));
+    const tracing = JSON.parse(fs.readFileSync(`./openlayers/vector-tiles/results/vector-vector/openlayers-trace-${i}.json` , 'utf8'));
   
     const traceScreenshots = tracing.traceEvents.filter(x => (
         x.cat === 'disabled-by-default-devtools.screenshot' &&
@@ -41,7 +44,7 @@ const fs = require('fs');
     ));
   
     traceScreenshots.forEach(function(snap, index) {
-      fs.writeFile(`leaflet-trace-screenshot-${index}.png`, snap.args.snapshot, 'base64', function(err) {
+      fs.writeFile(`openlayers-trace-${i}-screenshot-${index}.png`, snap.args.snapshot, 'base64', function(err) {
         if (err) {
           console.log('writeFile error', err);
         }
@@ -50,3 +53,4 @@ const fs = require('fs');
   
     await browser.close();
   })();
+}
